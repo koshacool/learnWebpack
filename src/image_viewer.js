18dcb376af10722ee16872eb21f5d0bf44094d63
koshacool <roman.kushytskyy@gmail.com>
2017-07-13 17:01:38 +0300

    a

--- src/image_viewer.js
+++
@@ -1,6 +0,0 @@
-
-const image = document.createElement('img');
-
-image.src = 'http://lorempixel.com/400/400';
-
-document.body.appendChild(image);
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,40 @@
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const VENDOR_LIBS = [
+	'react', 'lodash', 'redux', 'react-redux', 'react-dom',
+	'faker', 'react-input-range', 'redux-form', 'redux-thunk'
+];
 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: path.resolve(__dirname, 'build'),
-		filename: 'bundle.js',
-	},
-	module: {
-		rules: [
-			{
-				use: 'babel-loader',
-				test: /\.js$/
-			},
-			{
-				loader: ExtractTextPlugin.extract({
-					loader: 'css-loader'
-				}),
-				test: /\.css$/
-			}
-		]
-	},
-	plugins: [
-		new ExtractTextPlugin({
-      		filename: 'style.css',
-    	}) 
-	]
-
-
-
+  entry: {
+  	bundle: './src/index.js',
+  	vendor: VENDOR_LIBS
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
+  },
+  module: {
+  	rules: [
+  		{
+  			use: 'babel-loader',
+  			test: /\.js$/,
+  			exclude: /node_modules/
+  		},
+  		{
+  			use: ['style-loader', 'css-loader'],
+  			test: /\.css$/
+  		}
+  	]
+  },
+  plugins: [
+  	new webpack.optimize.CommonsChunkPlugin({
+  		names: ['vendor', 'manifest']
+  	}),
+  	new HtmlWebpackPlugin({
+  		template: 'src/index.html'
+  	})
+  ]
 };
